06a1d716e18bd9c4946189505b8641f9734472df
tienbian <tientranvu@gmail.com>
2015-08-06 16:03:19 +0700

    interface admin page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < ApplicationController
+  layout"adminstrator"
 before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   # GET /categories
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < ApplicationController
+  layout"adminstrator"
 before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
